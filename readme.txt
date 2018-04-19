ec50d1224edbca2f62150f180c5bb3a9376df498
Kenny <kmeadowsii@gmail.com>
2018-04-19 16:42:34 -0400

    first commit

---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== RM Version ===
+Contributors: kmiidev
+Donate link: http://www.hughlashbrooke.com/donate
+Tags: wordpress, plugin, version removal
+Requires at least: 4.3
+Tested up to: 4.5
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+
+== Description ==
+
+This plugin is designed to remove version number printing from wp_head to help protect wordpress.
+
+== Installation ==
+
+Installing "RM Version" can be done either by searching for "RM Version" via the "Plugins > Add New" screen in your WordPress dashboard, or by using the following steps:
+
+1. Upload the ZIP file through the 'Plugins > Add New > Upload' screen in your WordPress dashboard
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Changelog ==
+
+= 0.1 =
+* 2018-04-19
+* Initial release
+
+== Thanks ==
+Template for plugin provided by: Hugh Lashbrooke 
+https://github.com/hlashbrooke/WordPress-Plugin-Template
---
+++ rm-version.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Plugin Name: Remove Wordpress Version
+ * Version: 0.1
+ * Plugin URI: meadowsii.com
+ * Description: A plugin to hide your wordpress version.
+ * Author: Kenny
+ * Author URI: meadowsii.com
+ * Requires at least: 4.0
+ * Tested up to: 4.5
+ *
+ * Text Domain: rm-version
+ * Domain Path: /lang/
+ *
+ * @package WordPress
+ * @author Kenny
+ * @since 0.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// Load plugin class files
+require_once( 'includes/class-rm-version.php' );
+require_once( 'includes/class-rm-version-settings.php' );
+
+// Load plugin libraries
+require_once( 'includes/lib/class-rm-version-admin-api.php' );
+require_once( 'includes/lib/class-rm-version-post-type.php' );
+require_once( 'includes/lib/class-rm-version-taxonomy.php' );
+
+/**
+ * Returns the main instance of RM_Version to prevent the need to use globals.
+ *
+ * @since  1.0.0
+ * @return object RM_Version
+ */
+function RM_Version () {
+	$instance = RM_Version::instance( __FILE__, '1.0.0' );
+
+	if ( is_null( $instance->settings ) ) {
+		$instance->settings = RM_Version_Settings::instance( $instance );
+	}
+
+	return $instance;
+}
+
+RM_Version();
+
+// remove version from head
+remove_action('wp_head', 'wp_generator');
+
+// remove version from rss
+add_filter('the_generator', '__return_empty_string');
+
+// remove version from scripts and styles
+function shapeSpace_remove_version_scripts_styles($src) {
+	if (strpos($src, 'ver=')) {
+		$src = remove_query_arg('ver', $src);
+	}
+	return $src;
+}
+add_filter('style_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
